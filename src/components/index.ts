f19f6e4f78a640343b26dd640d9d792568ebcccc
Akezhan <akidulatini@gmail.com>
2024-03-01 11:20:46 +0600

    Create upcoming events card, create skeletons

---
+++ src/components/index.ts
@@ -0,0 +1,17 @@
+import UpcomingEvents from "./home/upcoming-events";
+import DealsChart from "./home/deals-chart"
+import AccordionHeaderSkeleton from "./skeleton/accordion-header"
+import KanbanSkeleton from "./skeleton/kanban"
+import LatestActivitiesSkeleton from "./skeleton/latest-activities";
+import ProjectCardSkeleton from "./skeleton/project-card";
+import UpcomingEventsSkeleton from "./skeleton/upcoming-events";
+
+export {
+  UpcomingEvents,
+  DealsChart,
+  AccordionHeaderSkeleton,
+  KanbanSkeleton,
+  LatestActivitiesSkeleton,
+  ProjectCardSkeleton,
+  UpcomingEventsSkeleton,
+}
